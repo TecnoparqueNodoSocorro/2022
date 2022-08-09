85d5742713ccf5778e94fa1cc6f4870494e37267
ofechaba2 <ofecha@misena.edu.co>
2022-08-09 17:05:57 -0500

    update tp

--- baricharApp/views/pages/front/modal.php
+++ baricharApp/views/pages/front/modal.php
@@ -14,24 +14,61 @@
                 <div class="container">
 
                     <div class="row">
-                        <div class="col-4" id="tarjetas" style="background-color:red ;padding: 10px;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:green ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:black ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:blue ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:red ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:green ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:black ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:blue ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:red ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:green ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:black ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:blue ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:red ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:green ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:black ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:blue ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:black ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
-                        <div class="col-4" style="background-color:blue ;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, inventore! Reprehenderit </div>
+                        <div id="tarjetas">
+                            <!--  -->
+                            <div class="row">
+
+
+                                <div class="col-4">
+                                    <div class="container-fluid">
+                                        <img class="imgtarjeta" src="views/images/productos/argolla.jpg" alt="star">
+                                    </div>
+                                </div>
+
+                                <!--  -->
+                                <div class="col-8">
+                                    <div class="container-fluid">
+                                        <h2 class="nombre">anillos de boda celeste</h2>
+                                    </div>
+                                    <div class="container-fluid">
+                                        <div class="detalleproducto">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere ullam sapiente incidunt nostrum quis molestiae ex inventore totam quam necessitatibus
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="container-fluid">
+                                        <div>
+                                            <h2 class="precioproducto">$150.000</h2>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                                <hr>
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <img class="logoE" src="views/images/productos/argolla.jpg" alt="">
+                                        </div>
+
+                                        <div class="col-7">
+                                            <p class="name_empresa">nombre de la empresa nombre de la empresa nombre de la empresa</p>
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="row">
+                                                <button type="button" class="btn btn-outline-warning btn-sm redondo"> <i class="bi bi-check2-square"></i></button>
+                                                <label class="labelbtn">Agregar</label>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--  -->
+
+                        <!--  -->
+
 
                     </div>
 
