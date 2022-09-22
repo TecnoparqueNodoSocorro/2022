7ea6ae45952c3e1e93d7244509ceb8291fa26f1d
ofechaba2 <ofecha@misena.edu.co>
2022-09-22 17:08:54 -0500

    actualizacion futurismobari

--- bariturismo/views/pages/home.php
+++ bariturismo/views/pages/home.php
@@ -3,76 +3,104 @@
 <hr>
 <hr>
 <hr>
+<div style="background-color: yellow;">
+
+    <div class="container1">
+        <input type="checkbox" id="btn-mas">
+        <div class="redes">
+            <a href="#" class="fa fa-youtube">Historia</a>
+        </div>
+        <div class="redes">
+            <a href="#" class="fa fa-facebook">Turismo</a>
+        </div>
+
+        <div class="redes">
+            <a href="#" class="fa fa-twitter">Restaurantes</a>
+        </div>
+        <div class="redes">
+            <a href="#" class="fa fa-pinterest">Hospedajes</a>
+        </div>
+        <div class="redes">
+            <a href="#" class="fa fa-pinterest">Generales</a>
+        </div>
+        <div class="btn-mas">
+            <label for="btn-mas" class="fa fa-plus"></label>
+        </div>
+    </div>
+</div>
 <div class="container container-barichara" style="background-color: blue;">
     <div class="row">
- <div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">Menu</span>
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+        <div class="container-fluid">
+            <div class="row flex-nowrap">
+                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
+                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                        <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline">Menu</span>
                         </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-                    </li>
-                
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-                    </li>
-                </ul>
-              
+                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link align-middle px-0">
+                                    <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
+                            </li>
+
+                            <li>
+                                <a href="#" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col py-3">
+                    Content area... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quasi sint quos, eligendi reiciendis iste? Cum officiis voluptates velit natus, ratione tempore consequatur assumenda et! Explicabo neque adipisci qui blanditiis!
+                </div>
             </div>
         </div>
-        <div class="col py-3">
-            Content area... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quasi sint quos, eligendi reiciendis iste? Cum officiis voluptates velit natus, ratione tempore consequatur assumenda et! Explicabo neque adipisci qui blanditiis!
-        </div>
     </div>
 </div>
-    </div>
+
+<hr>
+
+<hr>
 
 
-
-</div>
 <div class="container container-villanueva" style="background-color: red;">
- <div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">Menu</span>
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-                    </li>
-                
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-                    </li>
-                </ul>
-              
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline">Menu</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col py-3">
+                Content area... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quasi sint quos, eligendi reiciendis iste? Cum officiis voluptates velit natus, ratione tempore consequatur assumenda et! Explicabo neque adipisci qui blanditiis!
             </div>
         </div>
-        <div class="col py-3">
-            Content area... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quasi sint quos, eligendi reiciendis iste? Cum officiis voluptates velit natus, ratione tempore consequatur assumenda et! Explicabo neque adipisci qui blanditiis!
-        </div>
     </div>
-</div>
 
 </div>
 
